2658ab2eb1a4287aaafe8eb4c50eefe39c99424e
Levente Laurinyecz <47329402+levoskaa@users.noreply.github.com>
2019-10-10 22:40:43 +0200

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -74,19 +75,19 @@ app.get('/weather', (req, res) => {
     });
 });
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        res.send({
-            error: 'You must provide a search term!'
-        });
-        return;
-    }
+// app.get('/products', (req, res) => {
+//     if (!req.query.search) {
+//         res.send({
+//             error: 'You must provide a search term!'
+//         });
+//         return;
+//     }
 
-    console.log(req.query.search);
-    res.send({
-        products: []
-    });
-});
+//     console.log(req.query.search);
+//     res.send({
+//         products: []
+//     });
+// });
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
@@ -104,6 +105,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.');
 });
